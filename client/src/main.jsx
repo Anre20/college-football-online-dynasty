cb83eb1069a3f947fb766742daa7232e3f93a26c
Anre20 <a.borgesbrown@gmail.com>
2024-08-02 12:46:29 -0500

    updated-file-structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { UsersProvider } from './context/UserContext';
+import { UsersProvider } from './context/UserContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
